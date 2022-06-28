c2c48e4abd044446b77e61e1590820a92fbef6c8
haniefhelmi <87356220+haniefhelmi@users.noreply.github.com>
2022-06-28 19:10:29 +0700

    Add files via upload

---
+++ home.js
@@ -0,0 +1,45 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+
+
+
+
+
+function gantiterang(){
+  document.body.style.backgroundColor = "white";
+  document.getElementById("nav").style.backgroundColor = "rgb(255, 255, 255,0.3)";
+  document.getElementById('content').style.color = "black";
+  document.getElementById("ganti").style.color = "black";
+}
+
+function gantigelap(){
+  document.body.style.backgroundColor = "black";
+  document.getElementById("nav").style.backgroundColor = "rgb(0, 0, 0,0.3)";
+  document.getElementById('content').style.color = "white";
+  document.getElementById("ganti").style.color = "white";
+}
